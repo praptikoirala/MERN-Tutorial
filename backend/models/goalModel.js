659f631e4f4f91555c3b6d5a17ceb830367d9124
Prapti Koirala <praptikoirala6@gmail.com>
2023-08-30 17:33:25 +0545

    created REST API for goals

---
+++ backend/models/goalModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Goal', goalSchema)
